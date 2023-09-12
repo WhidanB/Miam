4ccaab24e07a99eebc9b435c687fcb10c93cca29
WhidanB <danyphone95@gmail.com>
2023-09-12 16:58:48 +0200

    some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='header'>
-        <h1>Miam Miam</h1>
+        <h1>🥗 Miam Miam 🥩</h1>
         </div>
     );
 };
