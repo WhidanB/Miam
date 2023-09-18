7de66c83e479b038b4c5503be69cf1eba75c4e6d
WhidanB <danyphone95@gmail.com>
2023-09-18 16:18:28 +0200

    suggestions ok

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react';
-import Embed from 'react-embed'; 
 import { Link } from 'react-router-dom';
+import Suggestion from './Suggestion';
 
 const Desc = (meal) => {
     const YTube = meal.meal.strYoutube.replace("watch?v=", "embed/");
-    console.log(YTube)
+    
     let Ingredient = [];
             let i = 1;
             while (true && i <= 20) {
@@ -22,49 +22,46 @@ const Desc = (meal) => {
         <div className='Recipee-container'>
             <div className='meal-wrapper'>
 
-            <div className='meal-container'>
-                <img src={meal.meal.strMealThumb} alt="" />
-                <div className='info'>
-                    <div>
-                        <h1>{meal.meal.strMeal}</h1>
-                        <p>{meal.meal.strArea}</p>
-                    </div>
-                    <iframe width="480" className='max-w-full' height="270" src={YTube} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>                </div>
+                <div className='meal-container'>
+                    <img src={meal.meal.strMealThumb} alt="" />
+                    <div className='info'>
+                        <div>
+                            <h1>{meal.meal.strMeal}</h1>
+                            <p>{meal.meal.strArea}</p>
+                        </div>
+                        <iframe width="480" className='max-w-full' height="270" src={YTube} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>                </div>
                 </div>
-                <div className='ing'>
-                    <h2>Ingredients</h2>
-                    <div className='ing-container'>
+                    <div className='ing'>
+                        <h2>Ingredients</h2>
+                        <div className='ing-container'>
 
-                    {Ingredient.map((ingredient, index) => {
-                        const imgName = ingredient[1].replace(' ', "%20")
-                        const imgURL = `https://www.themealdb.com/images/ingredients/${imgName}.png`
-                        return <Fragment key={`ing${index}`}>
+                        {Ingredient.map((ingredient, index) => {
+                            const imgName = ingredient[1].replace(' ', "%20")
+                            const imgURL = `https://www.themealdb.com/images/ingredients/${imgName}.png`
+                            return <Fragment key={`ing${index}`}>
+                    
+                            <div className='ingredients-container'>
+                            
+                                <img alt={ingredient[1]} src={imgURL} className='ingimg'/>
+                                <div >
+                                    <h3>{ingredient[0]}</h3>
+                                    <h2>{ingredient[1]}</h2>
+                                </div>
+                            </div>
+                            </Fragment>
+                                        })}
+                        </div>
+            
+                    </div>
+
+            
+                <div className='ins'>
+                    <h2>Instructions</h2>
+                    <p>{meal.meal.strInstructions}</p>
+                </div>
                 
-                    <div className='ingredients-container'>
-                    
-                        <img alt={ingredient[1]} src={imgURL} className='ingimg'/>
-                        <div >
-                            <h3>{ingredient[0]}</h3>
-                            <h2>{ingredient[1]}</h2>
-
-                    
-                    </div>
-                </div>
-
-
-            </Fragment>
-        })}
-        </div>
-            
-                </div>
-
-            
-            <div className='ins'>
-                <h2>Instructions</h2>
-                <p>{meal.meal.strInstructions}</p>
-            </div>
-
-            <p>Source: {meal.meal.strSource}</p>
+                <p>Source: {meal.meal.strSource}</p>
+            <Suggestion data={meal} />
             </div>
         </div>
     );
